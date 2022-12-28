f6d3852ea13aa4ccb6a9e6e66ca95748ce5a3d30
noorulhassan364 <noorulhassan364@gmail.com>
2022-12-28 09:07:13 +0500

    chnages

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.84.159.128:5000/api/v1",
-  // baseURL: "http://localhost:5000/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "http://3.84.159.128:5000/api/v1",
 });
 
 export default api;
